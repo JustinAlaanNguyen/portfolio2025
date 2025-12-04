57fb0a84191a5124626cd84cd54060b262a15081
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2025-12-04 18:47:24 -0500

    dec 4

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,21 +3,16 @@ import "./contact.css";
 
 export default function ContactPage() {
   return (
-    <>
-      {/* TOP SECTION — hills background */}
-      <div className="contact-bg-top">
-        <div className="contact-wrapper">
-          <h1 className="contact-title">CONTACT ME</h1>
-
-          <p className="contact-subtitle">
-            If you&apos;d like to get in touch, <br /> I&apos;d love to hear
-            from you!
-          </p>
-        </div>
+    <div className="contact-page-bg">
+      <div className="contact-wrapper-top">
+        <h1 className="contact-title">CONTACT ME</h1>
+        <p className="contact-subtitle">
+          If you&apos;d like to get in touch, <br /> I&apos;d love to hear from
+          you!
+        </p>
       </div>
 
-      {/* BOTTOM SECTION — background under the card */}
-      <div className="contact-bg-bottom">
+      <div className="contact-card-wrapper">
         <div className="contact-card">
           <form className="contact-form">
             <div className="input-group">
@@ -41,6 +36,6 @@ export default function ContactPage() {
           </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
